48f6e1267c89c4cfc59dc225707889f9167e8c5e
Nicolai Nielsen <NIcolaiaphichat@gmail.com>
2023-04-15 22:53:56 +0200

    did we c

--- gree.py
+++ gree.py
@@ -58,3 +58,69 @@ class Model:
 model = Model()
 classifier = model.model()
         
+        
+        import tkinter as tk
+import tkinter.scrolledtext as tkst
+
+class Model:
+    def __init__(self):
+        self.value1 = 0
+        self.value2 = 0
+        self.result = 0
+
+    def add(self):
+        self.result = self.value1 + self.value2
+
+class View(tk.Frame):
+    def __init__(self, parent, controller):
+        super().__init__(parent)
+        self.controller = controller
+        self.value1_label = tk.Label(self, text="Value 1:")
+        self.value1_label.pack(pady=5)
+        self.value1_entry = tk.Entry(self)
+        self.value1_entry.pack(pady=5)
+        self.value2_label = tk.Label(self, text="Value 2:")
+        self.value2_label.pack(pady=5)
+        self.value2_entry = tk.Entry(self)
+        self.value2_entry.pack(pady=5)
+
+        self.log = tkst.ScrolledText(self, height=4, width=50)
+        self.log.pack(pady=5)
+
+        self.add_button = tk.Button(self, text="Add", command=self.controller.add)
+        self.add_button.pack(pady=5)
+        self.result_label = tk.Label(self, text="Result: 0")
+        self.result_label.pack(pady=5)
+
+    def update(self, result):
+        self.result_label.config(text="Result: " + str(result))
+
+    def get_values(self):
+        value1 = int(self.value1_entry.get())
+        value2 = int(self.value2_entry.get())
+        return value1, value2
+
+    def log_input(self):
+        value1, value2 = self.get_values()
+        self.log.insert(tk.END, "Value 1: " + str(value1) + "\n")
+        self.log.insert(tk.END, "Value 2: " + str(value2) + "\n")
+        self.log.insert(tk.END, "------------------------\n")
+
+class Controller:
+    def __init__(self, root):
+        self.model = Model()
+        self.view = View(root, self)
+
+    def add(self):
+        self.view.log_input()
+        value1, value2 = self.view.get_values()
+        self.model.value1 = value1
+        self.model.value2 = value2
+        self.model.add()
+        self.view.update(self.model.result)
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    controller = Controller(root)
+    controller.view.pack()
+    root.mainloop()
